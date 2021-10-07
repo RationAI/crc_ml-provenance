9c3647bef09ce586894af43b45c236be9fa29651
Matej Gallo <422328@mail.muni.cz>
2021-10-07 15:06:42 +0200

    Uses fully qualified names for class definitions.

--- rationai/utils/class_handler.py
+++ rationai/utils/class_handler.py
@@ -1,6 +1,7 @@
 import importlib
 
-def get_class(module_name, class_name):
+def get_class(full_path):
     """Retrieves a class given fully qualified path for a class."""
+    module_name, class_name = full_path.rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
