782938ca440eedfada4a03b3c7909d0038853231
Matej Gallo <422328@mail.muni.cz>
2021-10-11 19:35:11 +0200

    Skeleton implementation

---
+++ rationai/training/executor/keras_executors.py
@@ -0,0 +1,9 @@
+# Standard Imports
+
+# Third-party Imports
+
+# Local Imports
+from histopat.rationai.training.base.executors import Executor
+
+class KerasExecutor(Executor):
+    pass
---
+++ rationai/training/models/keras_models.py
@@ -0,0 +1,10 @@
+# Standard Imports
+
+# Third-party Imports
+
+# Local Imports
+from histopat.rationai.training.base.models import Model
+
+
+class KerasModel(Model):
+    pass
