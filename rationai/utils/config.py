2f9c7a0a6bded670beaaaa8b15535daa8a8dd9d6
Matej Gallo <422328@mail.muni.cz>
2021-10-11 19:35:31 +0200

    Added base config class behaviour.

--- rationai/utils/config.py
+++ rationai/utils/config.py
@@ -1,3 +1,13 @@
 class ConfigProto():
-    def parse(self, cfg_path):
+    def __init__(self, json_filepath, json_dict=None):
+        self.json_filepath = json_filepath
+        if json_dict is None:
+            self.json_dict = self.__load_fron_file(json_filepath)
+        self.__parse()
+
+    def __load_fron_file(self, json_filepath):
+        with open(json_filepath, 'r') as json_finput:
+            return json.load(json_finput)
+
+    def __parse(self):
         raise NotImplemented("ConfigProto.parse() not implemented.")
