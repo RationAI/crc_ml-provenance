b8b938751d6087ebf4df3d02c972627abf669e6d
Vojtech Krajnansky <423126@mail.muni.cz>
2021-10-16 12:23:08 +0200

    Beautify ConfigProto

--- rationai/utils/config.py
+++ rationai/utils/config.py
@@ -1,21 +1,36 @@
+"""Configuration parsing utilities."""
 # Standard Imports
+from __future__ import annotations
 
 # Third-party Imports
 import json
 
 # Local Imports
+from pathlib import Path
+from typing import NoReturn
 
 
 class ConfigProto:
-    """ConfigProto consumes and parses JSON configuration file."""
+    """ConfigProto consumes and parses JSON configuration file.
+
+    Base class for configuration parsing classes.
+    """
     def __init__(self, json_dict: dict):
         self.config = json_dict
 
     @classmethod
-    def load_from_file(self, json_filepath):
+    def load_from_file(cls, json_filepath: Path) -> ConfigProto:
+        """Load the configuration from a JSON file.
+
+        Parameters
+        ----------
+        json_filepath : pathlib.Path
+            Path to JSON configuration file.
+        """
         with open(json_filepath, 'r') as json_finput:
             json_config = json.load(json_finput)
-        return self(json_config)
+        return cls(json_config)
 
-    def parse(self, config_fp):
+    def parse(self) -> NoReturn:
+        """Parses self.config into the actual configuration."""
         raise NotImplemented("ConfigProto.parse() not implemented.")
