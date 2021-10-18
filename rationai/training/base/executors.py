8c9a07b6d92adff7b954008569d3cbb8c1d332b7
Matej Gallo <422328@mail.muni.cz>
2021-10-18 21:59:12 +0200

    [Major] Keras Executor Implemented

--- rationai/training/base/executors.py
+++ rationai/training/base/executors.py
@@ -1,7 +1,12 @@
 from abc import ABC
 from abc import abstractmethod
 
+from rationai.utils.config import ConfigProto
+
 class Executor(ABC):
+    def __init__(self, config: ConfigProto):
+        self.config = config
+
     @abstractmethod
     def train(self):
         pass
--- rationai/training/executors/keras_executors.py
+++ rationai/training/executors/keras_executors.py
@@ -3,7 +3,79 @@
 # Third-party Imports
 
 # Local Imports
-from histopat.rationai.training.base.executors import Executor
+from typing import Generator
+from rationai.training.base.executors import Executor
+from rationai.training.base.models import Model
+from rationai.utils.config import ConfigProto
+from rationai.utils.class_handler import get_class
 
 class KerasExecutor(Executor):
-    pass
+    def __init__(self, config: ConfigProto):
+        super().__init__(config)
+
+    def train(self,
+              model: Model,
+              train_generator: Generator,
+              valid_generator: Generator = None) -> dict:
+        history = model.model.fit(x=train_generator,
+            validation_data=valid_generator,
+            epochs=self.config.epochs,
+            max_queue_size=self.config.max_queue_size,
+            workers=self.config.workers,
+            use_multiprocessing=self.config.use_multiprocessing,
+            callbacks=self.__get_callbacks()
+        )
+        return history.history
+
+    def predict(self, model: Model, generator: Generator):
+        return model.model.predict(x=generator,
+            max_queue_size=self.config.max_queue_size,
+            workers=self.config.workers,
+            use_multiprocessing=self.config.use_multiprocessing,
+            callbacks=self.__get_callbacks()
+        )
+
+    def __get_callbacks(self):
+        return [callback_cls(**callback_cfg)
+            for callback_cls, callback_cfg in zip(
+                self.config.callback_classes,
+                self.config.callback_configurations
+            )
+        ]
+
+    class Config(ConfigProto):
+        def __init__(self, json_dict: dict):
+            super().__init__(json_dict)
+            self.epochs = None
+
+            # Keras Misc Settings
+            self.max_queue_size  = None
+            self.workers = None
+            self.use_multiprocessing = None
+            self.callback_classes = None
+            self.callback_configurations = None
+
+        def parse(self):
+            # Training Params
+            self.epochs = self.config.get('epochs', None)
+
+            # Keras Misc Settings
+            self.max_queue_size = self.config.get('max_queue_size', 1)
+            self.workers = self.config.get('workers', 1)
+            self.use_multiprocessing = self.config.get('use_multiprocessing', False)
+
+            # Callback Parsing
+            callback_config = self.config.get(
+                'callbacks',
+                {'definitions': {}, 'configurations': {}}
+            )
+            self.callback_classes = [
+                get_class(callback) for callback in callback_config.get(
+                    'definitions',
+                    list()
+                )
+            ]
+            self.callback_configurations = callback_config.get(
+                'configurations',
+                [dict()] * len(self.callback_classes)
+            )
