f13cdb1d16d97551416e61580794d0e2d716d8b4
Jakub Hruska <445634@mail.muni.cz>
2021-11-18 21:52:21 +0100

    Raise version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rationai.histopat",
-    version="0.1.0",
+    version="0.1.1",
     description="AI Image Analysis Pipeline for Digital Pathology",
     packages=find_packages(),
     author="RationAI",
